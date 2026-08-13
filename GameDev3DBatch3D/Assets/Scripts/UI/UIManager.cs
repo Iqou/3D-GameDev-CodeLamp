d9afb8798a17def8e8adb18b62413b632441d6c0
BivaTumangkeng <tumangkeng.ac@gmail.com>
2026-08-13 21:37:27 +0700

    Adding HUD

--- GameDev3DBatch3D/Assets/Scripts/UI/UIManager.cs
+++ GameDev3DBatch3D/Assets/Scripts/UI/UIManager.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class UIManager : MonoBehaviour
 {
+    public GameObject pauseUI;
+    
     public void OnRestartPress()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnGameResumePress()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    public void OnGameExitPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnEnterPausePress()
+    {
+        pauseUI.SetActive(true);
     }
 }
